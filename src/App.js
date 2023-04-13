d8a4c241471912493f720baf4b393f16cd8ee2ab
RamandHawf <h.umerusman786@gmail.com>
2023-04-13 14:54:41 +0500

    Code ki

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import Aboutus from './Components/Aboutus';
+
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Aboutus/>
     </>
   );
 }
---
+++ src/Components/Aboutus.js
@@ -0,0 +1,272 @@
+import React from 'react';
+
+const Aboutus = () => {
+    return (
+      <div
+        className="container1"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          marginTop: "120px",
+          height: "632px",
+          width: "100%",
+          backgroundColor: "#1E1E1E",
+
+          //  textDecorationColor:'beige'
+        }}
+      >
+        <div className="one" style={{ width: "672px", height: "632px" }}>
+          <div
+            className="code1"
+            style={{
+              position: "absolute",
+              width: "317px",
+              height: "57px",
+              color: "#883BDE",
+              left: "86px",
+              top: "210px",
+              fontFamily: "Museomoderno",
+              fontStyle: "normal",
+              fontWeight: "700",
+              fontSize: "36px",
+              lineHeight: "57px",
+              display: "flex",
+              alignItems: "center",
+              textTransform: "uppercase",
+            }}
+          >
+            CRYPTO CONNECT
+          </div>
+          <div
+            className="code2"
+            style={{
+              position: "absolute",
+              width: "544px",
+              height: "112px",
+              left: "86px",
+              top: "267px",
+
+              fontFamily: "MuseoModerno",
+              fontStyle: "normal",
+              fontWeight: "700",
+              fontSize: "48px",
+              lineHeight: "56px",
+              /* or 117% */
+
+              display: "flex",
+              alignItems: "center",
+              textTransform: "uppercase",
+
+              color: "#FFFFFF",
+            }}
+          >
+            A NETWORK OF TRUST AND SECURITY
+          </div>
+          <div
+            className="code3"
+            style={{
+              position: "absolute",
+              width: "574px",
+              height: "76px",
+              left: "80px",
+              top: "403px",
+
+              fontFamily: "MuseoModerno",
+              fontStyle: "normal",
+              fontWeight: "400",
+              fontSize: "24px",
+              lineHeight: "38px",
+              display: "flex",
+              alignItems: "center",
+              textTransform: "capitalize",
+
+              color: "#FFFFFF",
+            }}
+          >
+            Crypterium Is The World Most Popular Way To Buy and Sell
+            Bitcoin,Etherium and LiteCoin
+          </div>
+          <button
+            style={{
+              position: "absolute",
+              width: "173px",
+              height: "60px",
+              left: "80px",
+              top: "519px",
+
+              background:
+                "linear-gradient(68.58deg, #883BDE 9%, #88D5DE 97.66%)",
+              boxShadow: "0px 4px 20px rgba(224, 50, 91, 0.09)",
+              borderRadius: "10px",
+            }}
+          >
+            Read More
+          </button>
+          <div
+            className="frame8"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "0px",
+              gap: "16px",
+
+              position: "absolute",
+              width: "281px",
+              height: "46px",
+              left: "80px",
+              top: "619px",
+            }}
+          >
+            <div
+              className="eclispe1"
+              style={{
+                width: "40px",
+                height: "40px",
+
+                background: "url(), #D9D9D9",
+                border: "1px solid #FFFFFF",
+
+                /* Inside auto layout */
+                borderRadius: "50%",
+                flex: "none",
+                order: 0,
+                flexGrow: 0,
+                margin: "0px -20px",
+              }}
+            ></div>
+            <div
+              className="eclispe2"
+              style={{
+                width: "40px",
+                height: "40px",
+
+                background: "url(), #D9D9D9",
+                border: "1px solid #FFFFFF",
+
+                /* Inside auto layout */
+                borderRadius: "50%",
+                flex: "none",
+                order: 0,
+                flexGrow: 0,
+                margin: "0px -20px",
+              }}
+            ></div>
+            <div
+              className="eclispe3"
+              style={{
+                width: "40px",
+                height: "40px",
+
+                background: "url(), #D9D9D9",
+                border: "1px solid #FFFFFF",
+
+                /* Inside auto layout */
+                borderRadius: "50%",
+                flex: "none",
+                order: 0,
+                flexGrow: 0,
+                margin: "0px -20px",
+              }}
+            ></div>
+                        <div
+              className="eclispe4"
+              style={{
+                width: "40px",
+                height: "40px",
+
+                background: "url(), #D9D9D9",
+                border: "1px solid #FFFFFF",
+
+                /* Inside auto layout */
+                borderRadius: "50%",
+                flex: "none",
+                order: 0,
+                flexGrow: 0,
+                margin: "0px -20px",
+              }}
+            ></div>
+         
+
+         <div className='communityqunatityinf8' 
+          style={{
+            display: "flex",
+            marginLeft:'10px',
+            flexDirection: "column",
+            alignItems: "flex-start",
+            padding: "0px",
+            width: "147px",
+            height: "46px",
+            flex: "none",
+            order: "1",
+            flexGrow: "0"
+          }}>
+
+<h1 style={{
+           width: "57px",
+           height: "38px",
+           
+           fontFamily: 'MuseoModerno',
+           fontStyle: "normal",
+           fontWeight: "700",
+           fontSize: "24px",
+           lineHeight: "38px",
+           display: "flex",
+           alignItems: "center",
+           
+           color: "#FFFFFF",
+           
+           
+           /* Inside auto layout */
+           
+           flex: "none",
+           order: 0,
+           flexGrow: 0,
+           margin: "-14px 0px",
+          }}>
+         47K+
+          </h1>
+          <h2 style={{
+           width: "147px",
+           height: "22px",
+           
+           fontFamily: 'MuseoModerno',
+           fontStyle: "normal",
+           fontWeight: "400",
+           fontSize: "14px",
+           lineHeight: "22px",
+           display: "flex",
+           alignItems: "center",
+           
+           color: "#FFFFFF",
+           
+           
+           /* Inside auto layout */
+           
+           flex: "none",
+           order: 0,
+           flexGrow: 0,
+        //    margin: "-14px 0px",
+          }}>
+         Community members
+          </h2>
+
+
+          </div>
+
+          </div>
+          
+         
+        </div>
+        <div
+          className="two"
+          style={{ width: "768px", height: "632px", backgroundColor: "yellow" }}
+        >
+          {/* CRYPTO CONNECT */}
+        </div>
+      </div>
+    );
+}
+
+export default Aboutus;
